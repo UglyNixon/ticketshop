08806fa864cb93e25d27953f15f3ab00d8c98654
UglyNixon <58419699+UglyNixon@users.noreply.github.com>
2020-10-14 17:22:59 +0300

    Add files via upload

--- catalog.js
+++ catalog.js
@@ -22,6 +22,42 @@ function createTeaters(teatr,name){
      })
      localStorage.setItem(name,JSON.stringify(teatr))
  }
+function createMovies(movie,name){
+ movie.forEach(element => {
+     let j=randomNum(1,3);  
+    for(let i=0;i<j;i++){
+        element.hall[i]={name:`hall_${i+1}`,places:[]}
+        let fixedPlaces = randomNum(6,20);
+       
+        rRow= randomNum(1,10);
+        for(let z=0;z<=rRow;z++){ let placesArray=[]
+            for (let i=0;i<=fixedPlaces;i++){
+                placesArray[i]={status:false,palceNumber:i+1,price:randomNum(500,2000)};
+            }
+            element.hall[i].places[z]=placesArray;
+        }
+    }
+     })
+     localStorage.setItem(name,JSON.stringify(movie))
+ }
+function createConcert(concert,name){
+ concert.forEach(element => {
+     let j=randomNum(1,3);  
+    for(let i=0;i<j;i++){
+        element.hall[i]={name:`hall_${i+1}`,places:[]}
+        let fixedPlaces = randomNum(6,20);
+       
+        rRow= randomNum(1,10);
+        for(let z=0;z<=rRow;z++){ let placesArray=[]
+            for (let i=0;i<=fixedPlaces;i++){
+                placesArray[i]={status:false,palceNumber:i+1,price:randomNum(500,2000)};
+            }
+            element.hall[i].places[z]=placesArray;
+        }
+    }
+     })
+     localStorage.setItem(name,JSON.stringify(concert))
+ }
 
  function randomDate(){
     let date = new Date();
@@ -35,16 +71,76 @@ function createTeaters(teatr,name){
 
 function filterHall (type,nazv){
   let arr = type.filter(elem=>elem.place==nazv)
-//   console.group()
-//   console.log(type)
-//   console.log(nazv)
-//   console.log(arr)
-//   console.log( arr[0].hall[randomNum(0,arr[0].hall.length-1)])
-//   console.groupEnd()
-
   return  arr[0].hall[randomNum(0,arr[0].hall.length-1)]
 
 }
+
+function createTeatEvents (type,number){
+    for (let i=0;i<number;i++){
+        let nazv =TeatrsNames[randomNum(0,TeatrsNames.length-1)]
+        teatrEvents[i]={
+            type:'teatr',
+            name : teatEventsNames[randomNum(0,teatEventsNames.length-1)],
+            place : nazv,
+            date :randomDate(),
+            hall: filterHall(type,nazv),
+           
+       
+            buyTicket: function ()  {
+                       console.log(this.name)
+                   },
+           showTicket : function () {
+                       console.log(this.place)
+                   }
+                   }
+
+        }
+    }
+function createMoviEvents (type,number){
+   
+    for (let i=0;i<number;i++){
+        let nazv =MoviesNames[randomNum(0,MoviesNames.length-1)]
+        movieEvents[i]={
+            type:'movie',
+            name : MoviesEventsNames[randomNum(0,MoviesEventsNames.length-1)],
+            place : nazv,
+            date :randomDate(),
+            hall: filterHall(type,nazv),
+           
+       
+            buyTicket: function ()  {
+                       console.log(this.name)
+                   },
+           showTicket : function () {
+                       console.log(this.place)
+                   }
+                   }
+
+        }
+    }
+
+   
+function createConcEvents (type,number){
+    for (let i=0;i<number;i++){
+        let nazv =ConcertNames[randomNum(0,ConcertNames.length-1)]
+        concertEvents[i]={
+            type:'concert',
+            name : concEventsNames[randomNum(0,teatEventsNames.length-1)],
+            place : nazv,
+            date :randomDate(),
+            hall: filterHall(type,nazv),
+           
+       
+            buyTicket: function ()  {
+                       console.log(this.name)
+                   },
+           showTicket : function () {
+                       console.log(this.place)
+                   }
+                   }
+
+        }
+    }
  /*                 
  ********************************               
   */
@@ -57,32 +153,16 @@ function filterHall (type,nazv){
  ************************
  */
 
- 
 
- 
- function createEvents (type,number){
-     for (let i=0;i<number;i++){
-         let nazv =TeatrsNames[randomNum(0,TeatrsNames.length-1)]
-         teatrEvents[i]={
-             name : teatEventsNames[randomNum(0,teatEventsNames.length-1)],
-             place : nazv,
-             date :randomDate(),
-             hall: filterHall(type,nazv),
-            
-        
-             buyTicket: function ()  {
-                        console.log(this.name)
-                    },
-            showTicket : function () {
-                        console.log(this.place)
-                    }
-                    }
-
-         }
-     }
     let teatrEvents=[];
+    let movieEvents=[];
+    let concertEvents=[];
     let TeatrsNames = ['МХАТ','Театр Эстрады','Театр на Таганке'];
     let teatEventsNames=['король лир','Щелкунчик','Кабала святош','Три сестры','Чайка','Мыши,Кай и зелененький']
+    let MoviesNames = ['КАРО','3Dmax'];
+    let MoviesEventsNames=['Бэтмен','Золушка','Дуное дело не хитрое','Зловещие мертвецы','Дети против волшебников']
+    let ConcertNames = ['Крокус Сити Холл',"Китайский телчик Джао да"];
+    let concEventsNames=['Metalica','Киркоров','Aphex Twin','Воровайки']
     let teaters = [
        {place: "МХАТ",   hall:[] ,imgSrc :''},
        {place: "Театр Эстрады",hall:[],imgSrc :''},
@@ -109,34 +189,33 @@ function filterHall (type,nazv){
         teaters = JSON.parse(localStorage.getItem('teaters'))
         }
         if (localStorage.getItem('movies')==null) {
-        createTeaters(movies,"movies")
+        createMovies(movies,"movies")
         }
         else {   
         movies = JSON.parse(localStorage.getItem('movies'))
         }
         if (localStorage.getItem('concerts')==null) {
-        createTeaters(concerts,"concerts")
+        createConcert(concerts,"concerts")
         }
         else {   
         concerts = JSON.parse(localStorage.getItem('concerts'))
         }
- createEvents(teaters,20)
-
-
-
+ createTeatEvents(teaters,randomNum(5,20))
+ createMoviEvents(movies,randomNum(10,30))
+ createConcEvents(concerts,randomNum(7,25))
 /*
-тип события 
+тип события +
 Название события;+
 Дата проведения;+
 Место проведения;+
-Зал;
+Зал;+
 Ряды;
 Места;
 Id;
 Цена;
 Статус (продан/свободен);
-Метод: Купить билет (ряд, место);
-Метод: Показать билет
+Метод: Купить билет (ряд, место);+
+Метод: Показать билет+
 */
 
 
